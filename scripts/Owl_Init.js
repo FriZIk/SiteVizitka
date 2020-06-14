6351e7aaf561876e3cdbf3e62ba25ab4fd6f3576
frizik <tester251100@gmail.com>
2020-06-14 17:41:45 +0300

    14.06.2020

--- scripts/Owl_Init.js
+++ scripts/Owl_Init.js
@@ -8,7 +8,25 @@ $(document).ready(function()
       smartSpeed: 1000,
       autoplayTimeout: 3500,
       // nav:true,
-      dots: true
+      dots: true,
+      responsiveClass:true,
+      responsive:{
+        0:{
+            items:1,
+            nav:false,
+            loop: true
+        },
+        600:{
+            items:2,
+            nav:false,
+            loop: true
+        },
+        1000:{
+            items:2,
+            nav:false,
+            loop: true
+        }
+    }
     }
   );
 });
